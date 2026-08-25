36390237a2f623cd97dcbf5765ddc64081520f6d
MehdiDevW555 <mehdi.creativecoder@gmail.com>
2026-08-25 18:52:48 +0100

    add toggel open and close dialog create book place manually in api create book place manually

--- resources/js/conponentSpotly/api/shopAdmin/dialogs/API_CREATE_BOOK_PLACE_MANUALLY.jsx
+++ resources/js/conponentSpotly/api/shopAdmin/dialogs/API_CREATE_BOOK_PLACE_MANUALLY.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React from 'react'
-import { changeBookPlaceManually, changeToggleChangeStatusService } from '../../../../redux/slices/shopAdmin/ShopAdmin_slice'
+import { changeBookPlaceManually, ChangeToggleDialogCreateTicket } from '../../../../redux/slices/shopAdmin/ShopAdmin_slice'
 
 function API_CREATE_BOOK_PLACE_MANUALLY(dispatch,full_name,phone,service_id) {
     dispatch(changeBookPlaceManually({loading:1}))
@@ -8,7 +8,8 @@ function API_CREATE_BOOK_PLACE_MANUALLY(dispatch,full_name,phone,service_id) {
   .then((data_r)=>{
     console.log(data_r)
         dispatch(changeBookPlaceManually({full_name: '', phone: '', service_id: '', loading: 0, }))
-        dispatch(changeToggleChangeStatusService());
+        // dispatch(changeToggleChangeStatusService());
+        dispatch(ChangeToggleDialogCreateTicket())
   })
   .catch((err)=>{
     console.log(err)
